3ad6af140a44008007c029fbac39d08139357e60
JuaNicolas <nicolas.juany@gmail.com>
2019-08-21 17:45:38 -0300

    done

--- main.js
+++ main.js
@@ -1,55 +1,32 @@
 /*
-Pedir al usuario mediante prompt que ingrese su género y luego su edad. 
-Luego mostrar un mensaje en consola que diga Sr., Sra. o Sx.según corresponda
-y que también evalue si es mayor de edad o no al momento de construir el mensaje.
-Por ejemplo si el usuario ingresa male y 16, el mensaje debería ser Sr. 
-usted es menor de edad no puede ingresar, si ingresa female y 34 el mensaje sería Sra. 
-usted es mayor de edad puede ingresar.
-
-prompt
-console.log
-if
+De un arreglo dado, recorrer numero por numero y decir si es numero primo o no.
 */
 
-var gender = prompt('Ingrese su genero [masculino, femenino u otros]')
-var age = prompt('Por favor, ingrese su edad.')
-var ageInt = parseInt(age)
-const ageAdult = 18
-var isMale = gender === 'masculino'
-var isFemale = gender === 'femenino'
-var isOthers = gender === 'otros'
-var isAdult = ageInt >= ageAdult
-var notAdult = ageInt < ageAdult
-var msgWelcome = 'Hola '
-var msgSir = 'Señor,'
-var msgMadam = 'Señora'
-var msgOthers = ','
-var msgAdult = ' usted es mayor de edad y puede ingresar.'
-var msgNotAdult = ' usted no es mayor de edad y no puede ingresar.'
+var listOfNumbers = [163, 181, 721, 7, 123, 45, 65, 77, 72, 456, 893];
+var isPrime = "Numero primo: ";
+var isWhole = "Numero entero: ";
 
-if (isAdult) {
-    if (isMale) {
-        console.log(msgWelcome + msgSir + msgAdult)
-        // console.log('1')
-    } else if (isFemale) {
-        console.log(msgWelcome + msgMadam + msgAdult)
-        // console.log('2')
-    } else if (isOthers) {
-        console.log(msgWelcome + msgOthers + msgAdult)
-        // console.log('3')
-    }
-} else if (notAdult) {
-    if (isMale) {
-        console.log(msgWelcome + msgSir + msgNotAdult)
-        // console.log('4')
-    } else if (isFemale) {
-        console.log(msgWelcome + msgMadam + msgNotAdult)
-        // console.log('5')
-    }
-    else if (isOthers) {
-        console.log(msgWelcome + msgOthers + msgNotAdult)
-        // console.log('6')
-    }
-} else {
-    console.log('Datos no validos. Ingrese nuevamente')
+for (let index = 0; index < listOfNumbers.length; index++) {
+  const element = listOfNumbers[index];
+  var divBy2 = listOfNumbers[index] % 2;
+  var divBy3 = listOfNumbers[index] % 3;
+  var divBy5 = listOfNumbers[index] % 5;
+  var divBy7 = listOfNumbers[index] % 7;
+  var divBy11 = listOfNumbers[index] % 11;
+  var divBy13 = listOfNumbers[index] % 13;
+  if (divBy2 === 0 && element != 2) {
+    console.log(isWhole + element);
+  } else if (divBy3 === 0 && element != 3) {
+    console.log(isWhole + element);
+  } else if (divBy5 === 0 && element != 5) {
+    console.log(isWhole + element);
+  } else if (divBy7 === 0 && element != 7) {
+    console.log(isWhole + element);
+  } else if (divBy11 === 0 && element != 11) {
+    console.log(isWhole + element);
+  } else if (divBy13 === 0 && element != 13) {
+    console.log(isWhole + element);
+  } else {
+    console.log(isPrime + element);
+  }
 }
