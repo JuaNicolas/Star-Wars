88d67102a7ac3d14762c3b5d37d07617e63bcc31
Nicolas Juanico <nicolas.juany@gmail.com>
2019-09-10 20:47:06 -0300

    create the solution

--- main.js
+++ main.js
@@ -1,116 +1,170 @@
+// !PRIMER EJERCICIO
+
 /*
+Crear y agregar nodos en una lista
+Usar los métodos y propiedades vistos en clase para crear tres nodos <li> con distintos nombres adentro, 
+agregarles a cada uno la clase list-group-item y agregarlos a la lista provista uno por uno.
 
-*SessionStorage:
-
-El sessionStorage es un objeto global que tiene el navegador y que nos va a permitir persistir información
-durante la sesión que el usuario tenga en la página. La sesión va a durar hasta que el usuario cierre la pestaña o el navegador,
-cuando lo haga eso va a borrar la información guardada en este objeto. 
-El espacio de almacenamiento es de aproximadamente 2.5 MB a 5 MB depende del navegador que estemos usando y actualmente esta 
-ampliamente soportado. Este objeto suele ser un buen reemplazo de la Cookies para algunos de sus casos de uso, como también para 
-almacenar datos como el Token de acceso o configuraciones del usuario. Al igual que un objeto la forma de ordenar los datos es 
-con un par key: value, solo que en este caso el value se guarda siempre en formato string.
-
-*LocalStorage
-
-El localStorage es un objeto global que tiene el navegador con las mismas características que tiene el SessionStorage, 
-la única diferencia es que su tiempo de duración es ilimitado por lo cual la información se mantiene guardada aun cuando se cierre 
-el navegador o la pestaña.
-
+//<ul id="mainList" class="list-group">
+//  <!-- Elementos de la lista -->
+//</ul>
+//
+//Datos de prueba:
+//
+//var texts = ['Ed', 'Edd', 'Eddy' ]
+//Documentación:
+//
+//List group Bootstrap
+//
+//CreateElement
+//
+//AppendChild
 */
 
-// Methods
+// Creo el array primaryCharacters y secondaryCharacters de la serie infantil 'Ed, Edd y Eddy'
+var primaryCharacters = ['Ed', 'Edd', 'Eddy']
+var secondaryCharacters = ['Rolf', 'Nazz', 'Kevin', 'Jimmy', 'Sarah', 'Johnny', 'Tablon']
+
+// Funcion para crear la primer lista.
+function firstList(array) {
+    for (let index = 0; index < array.length; index++) {
+
+        // Creo el ul
+        let primaryList = document.createElement('ul')
+
+        // Selecciono el ID
+        let listOne = document.getElementById('listOne')
+
+        // Creo el li del ul
+        let listItemNode = document.createElement('li')
+
+        // Ingreso en el li el contenido del array
+        listItemNode.innerHTML = array[index]
+
+        // Agrego al ul el li
+        primaryList.appendChild(listItemNode)
+
+        // Agrego al ID el ul creando con anterioridad
+        listOne.appendChild(primaryList)
+    }
+
+}
+
+// Ejecuta la primer funcion.
+firstList(primaryCharacters)
+
+// Funcion para crear la segunda lista.
+function secondList(array) {
+    for (let index = 0; index < array.length; index++) {
+
+        // Creo el ul
+        let secondaryList = document.createElement('ul')
+
+        // Selecciono el ID
+        let listTwo = document.getElementById('listTwo')
+
+        // Creo el li del ul
+        let listItemNode = document.createElement('li')
+
+        // Ingreso en el li el contenido del array
+        listItemNode.innerHTML = array[index]
+
+        // Agrego al ul el li
+        secondaryList.appendChild(listItemNode)
+
+        // Agrego al ID el ul creando con anterioridad
+        listTwo.appendChild(secondaryList)
+    }
+}
+
+// Ejecuta la segunda funcion.
+secondList(secondaryCharacters)
+
+
+// *PRUEBAS PRIMARIAS
+
+// for (let index = 0; index < primaryCharacters.length; index++) {
+
+//     let primaryList = document.createElement('ul')
+//     let listOne = document.getElementById('listOne')
+//     let listItemNode = document.createElement('li')
+
+//     listItemNode.innerHTML = primaryCharacters[index]
+//     primaryList.appendChild(listItemNode)
+//     listOne.appendChild(primaryList)
+// }
+
+// for (let index = 0; index < secondaryCharacters.length; index++) {
+
+//     let secondaryList = document.createElement('ul')
+//     let listTwo = document.getElementById('listTwo')
+//     let listItemNode = document.createElement('li')
+
+//     listItemNode.innerHTML = secondaryCharacters[index]
+//     secondaryList.appendChild(listItemNode)
+//     listTwo.appendChild(secondaryList)
+// }
+
+
+// !SEGUNDO EJERCICIO
 
 /*
-*SetItem
-El método setItem(key, value) recibe como parámetro una key y un valor y como resultado guarda el valor en la key 
-pasada como parámetro, si ya existe un valor lo pisa.
+Crear una función que cree un nodo alumno [Pieza del Workshop I]
+Crear una función que reciba como parámetro un objeto alumno y devuelva como resultado un nodo <li> con los datos del alumno.
+var studentNode = createStudentNode(newStudent)
+Resultado ejemplo:
 
-*GetItem
-El método getItem(key) recibe como parámetro una key y devuelve el valor almacenado en esa key, 
-en caso de que no exista devuelve null
-
-*RemoveItem
-El método removeItem(key) recibe como parámetro una key y elimina el valor almacenado en esa así como la key.
-
-*JSON.stringify
-Este método nos va a permitir convertir objetos de JavaScript a JSON, para de esta manera poder guardarlos en el 
-LocalStorage o enviarlos a través de una llamada HTTP a una API. El método recibe como parámetro un objeto de JavaScript y 
-devuelve un JSON en formato string.
-
-*JSON.parse
-Este método nos va a permitir convertir JSON a objetos de JavaScript, al hacer el proceso inverso del JSON.stringify()
-nos permite poder volver a usar en nuestro código el elemento. Esto lo vamos a utilizar tanto para recuperar los datos del 
-LocalStorage como para recibir información de un API.
-
-*/
-
-// Exercises
-
-/*
-Crear una función que permita guardar un Array en el localStorage
-La función tiene que recibir como parámetros una key y un Array y convertir el Array en JSON para guardarlo en el localStorge. 
-La función tiene que validar los parámetros que recibe, en caso de que alguno sea incorrecto no debe romper ni guardar nada.
+<li class="list-group-item" id="22999333">
+  <h1>
+    Juan Peréz
+  </h1>
+  <h3>
+    DNI: 22999333
+  </h3><p>
+    E-mail: juan@gmail.com
+  </p>
+</li>
 Datos de prueba:
 
-var testList = ['CARLOS','GERONIMO','NICOLAS','LUCAS']
-
-setLocalList('studentsList', testList) // Guarda la lista en el localStorage en formato JSON
+var student = {
+  firstName: 'Juan',
+  lastName: 'Peréz',
+  dni: 22999333,
+  email: 'juan@gmail.com'
+}
 Documentación:
 
+List group Bootstrap
+
+CreateElement
 */
 
-
-// Array de prueba.
-var testArray = ['CARLOS', 'GERONIMO', 'NICOLAS', 'LUCAS', 'PEDRO']
-
-// Funcion para verificar que el parametro pasado es un Array para luego integrarlo al localStorage.
-function setLocalList(_array, _keyValue) {
-
-    // Verifico si el parametro es un Array.
-    if (Array.isArray(_array) && typeof _keyValue === 'string') {
-
-        // Transformo el Array en un JSON.
-        let isJSON = JSON.stringify(_array)
-
-        // Integro al localStorage el JSON.
-        localStorage.setItem(_keyValue, isJSON);
-    }
-
-    // Devuelvo false si los datos son erroneos.
-    return false
+var student = {
+    firstName: 'Juan',
+    lastName: 'Perez',
+    dni: 22456481,
+    email: 'juan.perez@gmail.com'
 }
 
-// Inicializo la funcion.
-setLocalList(testArray, 'studentList')
+// Recorro el Objeto student
+for (var key in student) {
 
+    // Obtengo el elemento con el ID.
+    let studentList = document.getElementById('list-group')
 
-/*
-Crea una función que permita levantar una lista del localStorage
-La función tiene que recibir como parámetro una key y buscar en el localStorage por esa key devolviendo un objeto de JavaScript, 
-si no existe el elemento debe devolver un Array vacío. La función tiene que validar los parámetros que recibe, en caso de que alguno sea 
-incorrecto no debe romper ni borrar nada.
+    // Creo un li.
+    let studentListItem = document.createElement('li')
 
-var studentsList = getLocalList('studentsList') // Debe devolver siempre un Array [] vacío o con elementos [...]
-JSON.parse
+    // Agrego estilos de Bootstrap 4 al ul y al li.
+    studentList.classList.add('list-group')
+    studentListItem.classList.add('list-group-item')
 
-localStorage
-*/
+    // Cada recorrido toma la .value del objeto.
+    var studentValue = student[key]
 
-// Funcion para verificar que el parametro es un JSON, si lo es, transformarlo en JS.
-function getLocalList(_keyValue) {
+    // Ingreso en el li el value
+    studentListItem.innerHTML = studentValue
 
-    // Se extrae el posible JSON y se coloca en una variable local.
-    let jsonArray = localStorage.getItem(_keyValue)
-
-    // Compruebo si el parametro es un JSON.
-    if (jsonArray) {
-
-        // Retorno el Array
-        return JSON.parse(jsonArray)
-    }
-
-    // Retorno un Array vacio si jsonArray no era un JSON valido.
-    return jsonArray = []
+    // Adjunto al ul el li
+    studentList.appendChild(studentListItem)
 }
-
-getLocalList('studentList')
