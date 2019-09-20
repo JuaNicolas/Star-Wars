5076d22a746712d2030a6249350fc01f144a8cbf
Nicolas Juanico <nicolas.juany@gmail.com>
2019-09-19 21:19:07 -0300

    workshop

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
-/*
-!Primera parte
-DNI tiene que ser un número positivo y ser único (Que no coincida con ninguno ya agregado en la lista).
-Deberá poder eliminar un alumno ingresando el número de DNI, en caso de que no exista no borra nada.
-
-*/
-
 // Base de Datos de Alumnos para alojar en el Local Storage
-// var studentDB = [{
-//   name: 'Pedro',
-//   surname: 'Paez',
-//   dni: 38453829,
-//   email: 'PedroPaez@gmail.com'
-// }, {
-//   name: 'Pedro',
-//   surname: 'Paez',
-//   dni: 38453829,
-//   email: 'PedroPaez@gmail.com'
-// }]
-var studentDB = []
+var studentDB = [{
+  name: 'Pedro',
+  surname: 'Paez',
+  dni: 38453829,
+  email: 'PedroPaez@gmail.com'
+}, {
+  name: 'Pedro',
+  surname: 'Paez',
+  dni: 38453829,
+  email: 'PedroPaez@gmail.com'
+}]
+
+
+// var student7DB = []
 
 function convert2JSON() {
   // Convierto JS en JSON
@@ -39,12 +34,10 @@ function updateClass(isValid, element) {
   if (isValid) {
     element.classList.add('is-valid')
     element.classList.remove('is-invalid')
-    console.log('si')
     return element.value
   } else {
     element.classList.add('is-invalid')
     element.classList.remove('is-valid')
-    console.log('no')
   }
 }
 
@@ -93,27 +86,45 @@ inputDni.onblur = (e) => {
   // Condiciones para validar:
   let inputDni = e.target
   let inputDniValue = parseInt(e.target.value)
+
+
   let isValid = inputDniValue !== NaN
     && inputDniValue !== null
     && inputDniValue > 1000000
     && 100000000 > inputDniValue
+  // && isInDB(inputDniValue)
 
-  // Confirmar si un alumno esta en la DB con el DNI no se encuentra en la Base de Datos
-  studentDB.forEach(student => {
-    if (inputDniValue !== student.dni) {
-      updateClass(isValid, inputDni)
-    }
-  })
-
+  updateClass(isValid, inputDni)
 }
+
+// isInDB = (dni) => {
+//   studentDB.forEach(student => {
+//     if (dni !== student.dni) {
+//       return true
+//     } else {
+//       return false
+//     }
+//   })
+// }
 
 // Agregar alumno a la Base de Datos
 let addStudent = document.getElementById('addStudent')
+addStudent.disabled = false
 addStudent.onclick = () => {
   let name = inputName.value
   let surname = inputSurname.value
   let dni = parseInt(inputDni.value)
   let email = inputEmail.value
+
+  let validName = inputName.classList.contains('is-valid')
+  let validSurname = inputSurname.classList.contains('is-valid')
+  let validDni = inputDni.classList.contains('is-valid')
+  let validEmail = inputEmail.classList.contains('is-valid')
+  let isValid = validName
+    && validSurname
+    && validDni
+    && validEmail
+  // && isInDB(name)
 
   student = {
     name,
@@ -122,63 +133,31 @@ addStudent.onclick = () => {
     email
   }
 
-  // 
-  back4JSON()
-  studentDB.push(student)
-  convert2JSON()
+  if (isValid) {
+    console.log('es valido')
+    back4JSON()
+    studentDB.push(student)
+    convert2JSON()
+  } else {
+    addStudent.disabled = true
+    console.log('acaaa')
+  }
+}
+
+window.onload = () => {
+
+  let studentList = document.querySelector('.col ul')
+  let studentNode = document.createElement('li')
+
+  dataBase = localStorage.getItem('DATA_BASE')
+
+  for (let index = 0; index < dataBase.length; index++) {
+    const element = dataBase[index];
+
+    console.log(element)
+    studentNode.innerHTML = 'hola'
+    studentList.appendChild(studentNode)
+  }
 }
 
 
-
-
-
-
-/*
-
-Tods los cambios tienen que quedar guardados en el LocalStorage, en el caso de que ya existan alumnos
-guardados los tiene que mostrar al inicio del programa.
-
-Deberá mostrar siempre una lista en pantalla con los alumnos cargados en el sistema que tendrá que ser actualizada en cada cambio.
-
-Para deshabilitar el botón hay que poner en true la propiedad disabled en el botón y para habilitarlo hay que cambiarla a false.
-Ej: document.getElementById('myButton').disabled = true.
-
-Piezas a utilizar:
-
-HTML -> Desafío clase 01
-Función setLocalList -> Ejercicio clase 07
-Función getLocalList -> Desafío clase 07
-Función createStudentNode -> Desafío clase 08
-Documentación:
-
-Listas de Bootstrap
-
-Diagramas provistos en esta misma carpeta mostrando los flujos de AltaEstudiante y BajaEstudiante
-
-*/
-
-
-/*
-
-!Segunda parte
-
-El formulario deberá también incluir los siguientes campos y validarlos:
-
-Apellido es un campo opcional.
-Email es un campo obligatorio y tiene que tener al menos un @ y un ..
-Deberá permitir buscar un alumno ingresando un texto encontrando coincidencias parciales en nombre y apellido, sin importar mayúsculas o
-minúsculas.
-
-Video ejemplo
-
-Piezas a utilizar:
-
-Función searchStudentIndexByText -> Desafío clase 04
-Función includesText -> Desafío clase 06
-Función validateEmailField -> Desafío clase 09
-Documentación:
-
-Listas de Bootstrap
-
-Diagrama provisto en esta misma carpeta mostrando el flujo BúsquedaEstudiante
-*/
