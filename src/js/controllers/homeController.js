18c44ee2dad306d0e4f26a6ea4b00e5b2a87cd28
JuaNicolas <nicolas.juany@gmail.com>
2019-10-18 13:07:54 -0300

    home fix

--- src/js/controllers/homeController.js
+++ src/js/controllers/homeController.js
@@ -1,8 +1,8 @@
 import saveUser from "../utils/home/saveUser"
 import { validName } from "../utils/contact/validFields"
 import { getLocalList } from "../utils/localStorage/localStorage"
-import deleteUser from "../utils/home/deleteUser"
 import userNode from "../utils/home/userNode"
+import saveChar from "../utils/people/saveChar"
 
 export default function homeController() {
 
@@ -10,6 +10,7 @@ export default function homeController() {
   // Abrir modal o cerrar si hay usuario.
 
   let userExist = getLocalList('user').length !== 0
+  let userCharList = getLocalList('chars').length !== 0
 
   if (userExist) {
 
@@ -19,22 +20,26 @@ export default function homeController() {
 
     userNode(userObjName)
 
-    // let userNode = $('<p id="user" class="h1 font-weight-bolder">').text(`Bienvenido! ${user[0].userName}`)
-    // let userDelete = $('<button class="btn btn-danger deleteUser">').text('eliminar usuario')
-    // $('.userCol').append(userNode).append(userDelete)
+  } else if (userCharList) {
+
+    let userNode = $('<p id="user" class="h1 font-weight-bolder">').text(`Bienvenido, usted no se ha registrado!`)
+    let userAdd = $('<button class="btn btn-success openModal">').text('Agregar usuario')
+    $('.userCol').append(userNode).append(userAdd)
+
+    $('.openModal').off().click(function () {
+      $('.modal').modal('show')
+      $('#newUser').blur(function () {
+        validName($(this))
+      }).keyup(function () {
+        validName($(this))
+      })
+
+      saveUser()
+    })
 
   } else {
 
     $('.modal').modal('show')
-
-
-    $('#newUser').blur(function () {
-      validName($(this))
-    }).keyup(function () {
-      validName($(this))
-    })
-
-    saveUser()
 
     $('.close').off().one('click', function () {
       let userNode = $('<p id="user" class="h1 font-weight-bolder">').text(`Bienvenido, usted no se ha registrado!`)
@@ -42,12 +47,17 @@ export default function homeController() {
       $('.userCol').append(userNode).append(userAdd)
 
       $('.openModal').off().click(function () {
+       
         $('.modal').modal('show')
+        $('#newUser').blur(function () {
+          validName($(this))
+        }).keyup(function () {
+          validName($(this))
+        })
+
+        saveUser()
       })
     })
-
-
-
   }
 
 }
--- src/js/utils/home/userNode.js
+++ src/js/utils/home/userNode.js
@@ -2,14 +2,12 @@ import deleteUser from "./deleteUser"
 
 export default function userNode(_user) {
 
-
     // Removes the addUser html
     $('.openModal').remove()
     $('.h1').remove()
     
     let userNode = $('<p id="user" class="h1 font-weight-bolder">').text(`Bienvenido! ${_user}`)
     let userDelete = $('<button class="btn btn-danger deleteUser">').text('eliminar usuario')
-
 
     $('.userCol').append(userNode).append(userDelete)
 
